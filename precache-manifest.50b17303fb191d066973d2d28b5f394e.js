c24f410d937bb97efb4cfe2f79d9d74dc75916fb
Roman <gnatyuk.info@gmail.com>
2020-04-18 13:06:48 +0300

    Updates

---
+++ precache-manifest.50b17303fb191d066973d2d28b5f394e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e68c896c37ad5fde425a64d5217253c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86ee88e0181420c19e7b",
+    "url": "/static/css/main.a320396c.chunk.css"
+  },
+  {
+    "revision": "ce5c6bac53f77bba14f3",
+    "url": "/static/js/2.9b6ab1a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b6ab1a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ee88e0181420c19e7b",
+    "url": "/static/js/main.ad597e3f.chunk.js"
+  },
+  {
+    "revision": "6464bd9f5e20cf161ffb",
+    "url": "/static/js/runtime-main.36e497e6.js"
+  }
+]);
